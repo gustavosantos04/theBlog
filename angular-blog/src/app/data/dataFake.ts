6bab306ef366cf89d9c48f25d44ab354a8bb36fe
Gustavo Santos <santosgustavo0504@gmail.com>
2024-04-15 22:11:30 -0300

    new project angular

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Aprenda a investir agora!",
+        "description": "Começe a investir hoje com as melhores dicas do mundo dos investimentos.",
+        "photoCover": "https://organismobrasil.com.br/wp-content/uploads/2023/05/Diversificacao-de-investimentos-proteja-seu-patrimonio-e-maximize-seus-lucros-1.png"
+    },
+    
+    {
+        "id": 2,
+        "title": "Todas as informações do Bitcoin",
+        "description": "aprenda tudo neste exato momento.",
+        "photoCover": "https://i-invdn-com.investing.com/news/moved_LYNXMPEI850WX_L.jpg"
+    }
+]
